3766eaacb820e4a60a95ec348368904b5e0872ae
Defy <61808223+De-y@users.noreply.github.com>
2023-03-04 14:34:33 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,40 @@
+"""
+dpyrandmeme init file:
+
+This is where the code is stored for the meme and discord-embed logic.
+"""
 
 import aiohttp
 import discord
-import random2
+import random
 
 
 async def pyrandmeme():
-    pymeme = discord.Embed(title="Meme", description="Meme Request", color=0xe91e63)
+    lists_for_memes = ['https://www.reddit.com/r/GoCommitDie/rising.json?sort=rising',
+                       'https://www.reddit.com/r/okbuddyretard/top.json?sort=top', 
+                       'https://www.reddit.com/r/memes/rising.json?sort=rising', 
+                       'https://www.reddit.com/r/meme/rising.json?sort=rising', 
+                       'https://www.reddit.com/r/dank_meme/rising.json?sort=rising', 
+                       'https://www.reddit.com/r/AdviceAnimals/rising.json?sort=rising',
+                       'https://www.reddit.com/r/deepfriedmemes/rising.json?sort=rising',
+                       'https://www.reddit.com/r/dankchristianmemes/rising.json?sort=rising',
+                       'https://www.reddit.com/r/terriblefacebookmemes/rising.json?sort=rising',
+                       'https://www.reddit.com/r/prequelmemes/rising.json?sort=rising',
+                       'https://www.reddit.com//r/garlicbreadmemes/rising.json?sort=rising',
+                       'https://www.reddit.com/r/offensivememes/rising.json?sort=rising',
+                       'https://www.reddit.com/r/arabfunny/rising.json?sort=rising',
+                       'https://www.reddit.com/r/darkmemes/rising.json?sort=rising',
+                       'https://www.reddit.com/r/antimeme/rising.json?sort=rising',
+                       'https://www.reddit.com/r/madlads/rising.json?sort=rising',
+                       'https://www.reddit.com/r/funny/rising.json?sort=rising',]
+    
+
     async with aiohttp.ClientSession() as cs:
-        async with cs.get('https://www.reddit.com/r/memes/new.json?sort=hot') as r:
+        async with cs.get(lists_for_memes[random.randint(0, 16)]) as r:
             res = await r.json()
-            pymeme.set_image(url=res['data']['children'][random2.randint(0, 25)]['data']['url'])
+            var=res['data']['children'][random.randint(0, 16)]['data']
+            pymeme = discord.Embed(url="https://www.reddit.com" + var['permalink'], title="**" + var['title'] + "**", color=0xe91e63)
+            pymeme.set_image(url=var['url'])
+            pymeme.set_footer(text='Post made by u/' + var['author'] + ' and posted in subreddit r/' + var['subreddit'] + '.')
             return pymeme
         await pyrandmeme()
